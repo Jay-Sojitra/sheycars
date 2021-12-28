415b9bf404c99f947e2765c99fae1944b7c9f2de
Jay <jaysojitra1011@gmail.com>
2021-12-29 01:38:06 +0530

    last final commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Navigate, Route, Routes, useNavigate} from 'react-router-dom';
+import { BrowserRouter as Router,  Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
--- client/src/pages/Protect.js
+++ client/src/pages/Protect.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React  from 'react'
 import { Navigate } from 'react-router-dom';
 
 function Protect(props) {
