ebf51c62a382d5a5b0b716fa2dff8ccb3b9dc70a
Jay <jaysojitra1011@gmail.com>
2021-12-29 02:03:53 +0530

    final commit

--- server.js
+++ server.js
@@ -10,19 +10,19 @@ app.use('/api/cars', carsRoute);
 app.use('/api/users', userRoute);
 app.use('/api/bookings', bookingsRoute);
 
-// const path = require('path')
+const path = require('path')
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 
-//     app.use('/', express.static('client/build'))
+    app.use('/', express.static('client/build'))
 
-//     app.get('*', (req, res) => {
+    app.get('*', (req, res) => {
 
-//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 
-//     })
+    })
 
-// }
+}
 
 
 const port = process.env.PORT || 5000;
